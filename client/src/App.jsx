9632c22b36b39ce8e88174277a66450c5bede12d
gulfufu <67294394+um24@users.noreply.github.com>
2025-04-15 12:29:43 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://your-render-url.onrender.com');
 
 const contractList = ['Queens', 'King of Hearts', 'Diamonds', 'Slaps', 'Trix'];
 
